cc0d3296163634c72bbd1a104a5f88442c90d078
Nick Robinson <nick@isengard.io>
2015-10-10 11:48:06 -0400

    Updating business logic

--- src/hi.rb
+++ src/hi.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
+require 'twilio-ruby'
 
 set :bind, '0.0.0.0'
 
-get '/' do
-  "Hello World #{params[:name]}".strip
+get '/hello-monkey' do
+  Twilio::TwiML::Response.new do |r|
+    r.Say 'Hello Monkey'
+  end.text
+end
+
+get '/hello-frankie' do
+  Twilio::TwiML::Response.new do |r|
+    r.Say 'Hello Frankie'
+  end.text
 end
