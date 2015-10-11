06c43396bc292b87576fe9053b68fd70d23b2330
Nick Robinson <nick@isengard.io>
2015-10-10 20:14:44 -0400

    Try to respond to the user by name

--- src/hi.rb
+++ src/hi.rb
@@ -5,8 +5,13 @@ set :bind, '0.0.0.0'
 set :port, 80
 
 get '/hello-monkey' do
+  people = {
+      '+12059138469' => 'Frankie Robinson',
+      '+12054109722' => 'Nick Robinson'
+  }
+  name = people[params['From']] || 'Monkey'
   Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello Monkey'
+    r.Say "Hello #{name}"
   end.text
 end
 
